2d412bf773ea4c6e2274546b2a24b8154119baa8
David Federspiel <dfederspie@alloyacorp.org>
2019-08-06 09:45:39 -0400

    try: gulp build before runing app

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var port = process.env.PORT || 1337;
-
+const port = process.env.PORT || 1337;
 var express = require('express');
 var path = require('path');
 var app = express();
@@ -7,6 +6,8 @@ app.use(express.static('dist'))
 var router = express.Router();
 let jsonServer = require('json-server');
 var jsonData = require('./src/data/generate.js');
+
+const appRoot = path.resolve(__dirname);
 
 var json_server = jsonServer.create({
     verbosity: {
@@ -19,6 +20,9 @@ json_server.use(jsonServer.router(jsonData()));
 router.use('/api', json_server);
 
 app.use('/api', router);
+app.use(['/discover*'], function (req, res) {
+    res.sendFile(appRoot + '/dist/index.html');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
