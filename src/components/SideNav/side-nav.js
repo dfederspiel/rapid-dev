404772e9639cae263518bf8354c108c967a06352
David Federspiel <david@federnet.com>
2019-08-07 09:01:06 -0400

    feat: linked transactions page

--- src/components/SideNav/side-nav.js
+++ src/components/SideNav/side-nav.js
@@ -49,7 +49,7 @@ export default class SideNav extends React.Component {
             <div className={this.getClasses()}>
                 <div className="side-nav-upper">
                     <a className="active"><FontAwesome style={{ color: 'gray' }} name='home' size="lg" />{!this.state.minimal && <label>Home Overview</label>}</a>
-                    <a><FontAwesome style={{ color: 'gray' }} name='laptop' size="lg" />{!this.state.minimal && <label>Transactions</label>}</a>
+                    <a href="/discover/transactions"><FontAwesome style={{ color: 'gray' }} name='laptop' size="lg" />{!this.state.minimal && <label>Transactions</label>}</a>
                     <a><FontAwesome style={{ color: 'gray' }} name='money-bill-alt' size="lg" />{!this.state.minimal && <label>Liquidity</label>}</a>
                     <a><FontAwesome style={{ color: 'gray' }} name='chart-pie' size="lg" />{!this.state.minimal && <label>Investments</label>}</a>
                 </div>
