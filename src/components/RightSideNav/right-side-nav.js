e340c9e8875d3c35e6023e60ec9ad21e9a034144
Benjamin Bakle <bbakle@apterainc.com>
2019-08-17 10:32:40 -0400

    wip: refactor right side navigation

--- src/components/RightSideNav/right-side-nav.js
+++ src/components/RightSideNav/right-side-nav.js
@@ -17,9 +17,79 @@ export default class RightSideNav extends React.Component {
             </div>
         )
     }
+
+    renderClose = () => {
+        return (
+            <div className="alloya-close">
+                <button onClick={this.props.toggleRightSideNav}>
+                    <span className="title">CLOSE</span>
+                    <span className="icon"><FontAwesome name="times" /></span>
+                </button>
+            </div>
+        )
+    }
+
+    renderSettings = () => {
+        return (
+            <ul className="settings">
+                <NavLinkWithLevels
+                    title="Administration"
+                    showCaret={true}
+                    links={[{ href: '/', title: "Admin Level 2" }, { href: '/', title: "Admin Level 2" }]}
+                />
+                <NavLinkWithLevels
+                    title="Corporate Tool Box"
+                    showCaret={true}
+                    links={[{ href: '/', title: "Corporate Level 2" }, { href: '/', title: "Corporate Level 2" }]}
+                />
+                <NavLinkWithLevels
+                    title="Contract Management"
+                    showCaret={true}
+                    links={[{ href: '/', title: "Contract Level 2" }, { href: '/', title: "Contract Level 2" }, { href: '/', title: "Contract Level 2" }, { href: '/', title: "Contract Level 2" }]}
+                />
+            </ul>
+        )
+    }
+
+    renderInfo = () => {
+        return (
+            <ul className="info">
+                <NavLink title="Training and Events" href="/" />
+                <NavLink title="KnowledgeCenter" href="/" />
+                <NavLink title="Premier View FAQ's" href="/" />
+            </ul>
+        )
+    }
+
+    renderMember = () => {
+        return (
+            <ul className="member">
+                <NavLink title="Transaction Cutoff Times" href="/" />
+                <NavLink title="Contact Alloya" href="/" />
+                <NavLink title="Member Pay" href="/" />
+                <NavLink title="Alloya Home" href="/" />
+            </ul>
+        )
+    }
+
+    renderSignOut = () => {
+        return (
+            <ul>
+                <NavLink title="Sign Out" href="/" />
+            </ul>
+        )
+    }
+
     render() {
         const { toggleRightSideNav } = this.props;
-        const { renderNotifications } = this;
+        const {
+            renderNotifications,
+            renderClose,
+            renderSettings,
+            renderInfo,
+            renderMember,
+            renderSignOut
+        } = this;
 
         return (
             <div className="right-side-nav">
@@ -28,43 +98,11 @@ export default class RightSideNav extends React.Component {
                     <FontAwesome name='ellipsis-h' size="lg" />
                 </button>
                 <div className="right-side-menu">
-                    <div className="close-right-nav">
-                        <button onClick={toggleRightSideNav}>
-                            <span className="title">CLOSE</span>
-                            <span className="icon"><FontAwesome name="times" /></span>
-                        </button>
-                    </div>
-                    <ul className="settings">
-                        <NavLinkWithLevels
-                            title="Administration"
-                            showCaret={true}
-                            links={[{ href: '/', title: "Admin Level 2" }, { href: '/', title: "Admin Level 2" }]}
-                        />
-                        <NavLinkWithLevels
-                            title="Corporate Tool Box"
-                            showCaret={true}
-                            links={[{ href: '/', title: "Corporate Level 2" }, { href: '/', title: "Corporate Level 2" }]}
-                        />
-                        <NavLinkWithLevels
-                            title="Contract Management"
-                            showCaret={true}
-                            links={[{ href: '/', title: "Contract Level 2" }, { href: '/', title: "Contract Level 2" }, { href: '/', title: "Contract Level 2" }, { href: '/', title: "Contract Level 2" }]}
-                        />
-                    </ul>
-                    <ul className="info">
-                        <NavLink title="Training and Events" href="/" />
-                        <NavLink title="KnowledgeCenter" href="/" />
-                        <NavLink title="Premier View FAQ's" href="/" />
-                    </ul>
-                    <ul className="member">
-                        <NavLink title="Transaction Cutoff Times" href="/" />
-                        <NavLink title="Contact Alloya" href="/" />
-                        <NavLink title="Member Pay" href="/" />
-                        <NavLink title="Alloya Home" href="/" />
-                    </ul>
-                    <ul>
-                        <NavLink title="Sign Out" href="/" />
-                    </ul>
+                    {renderClose()}
+                    {renderSettings()}
+                    {renderInfo()}
+                    {renderMember()}
+                    {renderSignOut()}
                     <ul>
                         <NavLink title="BACK TO INDEX" href="/" />
                     </ul>
