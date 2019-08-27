dac8493da31546aca7d5152bdfc8bb768fbc40ef
Benjamin Bakle <bbakle@apterainc.com>
2019-08-27 15:19:52 -0400

    revert: left nav to onclick instead of hover. better ui

--- src/components/NavLinkWithLevels/nav-link-with-levels.js
+++ src/components/NavLinkWithLevels/nav-link-with-levels.js
@@ -44,13 +44,13 @@ export default class NavLinkWithLevels extends React.Component {
 
         return (
             <li className={`nav-link-with-levels ${active ? "active" : ""}`} ref={node => this.node = node}>
-                <a className="link" href="javascript:void(0)" onMouseEnter={toggle}  onMouseLeave={toggle}>
+                <a className="link" href="javascript:void(0)" onClick={toggle}>
                     <div className="title">{title}</div>
                     {showCaret && <Icon name="caret" />}
                 </a>
                 {
                     icon &&
-                    <a className="active-trigger" href="javascript:void(0)" onMouseEnter={toggle}  onMouseLeave={toggle}>
+                    <a className="active-trigger" href="javascript:void(0)" onClick={toggle}>
                         <Icon name={icon} size="lg"/>
                     </a>
                 }
