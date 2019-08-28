251239dfa0dd51cb462fb020addc520b3a20c003
Benjamin Bakle <bbakle@apterainc.com>
2019-08-28 09:02:47 -0400

    feat: logo as image link

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -4,8 +4,9 @@ export default class Logo extends React.Component {
     render() {
         return (
             <div className="logo">
-                <span>alloya</span>
-                <span className="sub">PremierView</span>
+                <a href="/">
+                    <img src="https://www.alloyacorp.org/wp-content/uploads/sites/2/2018/05/logo-new.png" alt="alloya premier view" />
+                </a>
             </div>
         );
     }
