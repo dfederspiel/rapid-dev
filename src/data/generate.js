f753d0deb5ca4e0e744706aa70cf76d8258eff55
David Federspiel <david@federnet.com>
2021-08-06 17:39:51 -0400

    clean: improve default build and documentation

--- src/data/generate.js
+++ src/data/generate.js
@@ -1,6 +1,6 @@
 /** Data generator script for mock service.
  * 
- * Referenced during PUG transpilation to generate random content for static files  
+ * Referenced during pug transpilation to generate random content for static files  
  * Consumed by json-server middleware (Browsersync and Node) to inject a mock API 
  * 
  * */
