599ec29410e552ee684db0ff9a337fd9ea9bcd3f
David Federspiel <david@federnet.com>
2021-08-14 01:11:35 -0400

    try: serverless functions

---
+++ api/Articles/index.js
@@ -0,0 +1,77 @@
+module.exports = async function (context, req) {
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: [
+            {
+                "id": 0,
+                "title": "Fish alarm robust mindshare International turn-key gold",
+                "date": "2020-12-06T09:58:21.030Z",
+                "authorId": 1,
+                "body": "qui neque accusamus minus distinctio vero in facere fugit quia molestiae earum possimus voluptatem quod voluptatem tenetur ipsum sunt atque quisquam ratione aut voluptas nulla ipsa dolor deleniti beatae similique aut laborum fugit corporis eaque error cum animi deserunt occaecati delectus corporis corrupti",
+                "author": {
+                    "id": 1,
+                    "firstName": "Lottie",
+                    "lastName": "Koelpin",
+                    "bio": "maxime enim voluptas aut dolor aut inventore nostrum est architecto aut a provident maiores quod et eius similique id tenetur eum rerum voluptatem dolores commodi",
+                    "website": "http://kaley.net"
+                }
+            },
+            {
+                "id": 1,
+                "title": "zero neural COM state online Metal AI redundant calculating Checking",
+                "date": "2021-04-30T07:40:38.699Z",
+                "authorId": 2,
+                "body": "ut voluptas rerum debitis neque delectus dolorum et quia et magnam laudantium eum blanditiis et rerum suscipit velit quos eos aut voluptatem illo ducimus dolor sit nostrum est dolorum aut aspernatur qui similique et sed deserunt voluptas autem tempora beatae et dolor enim assumenda est at odio rerum quisquam velit magnam tempore ut ea nostrum totam quaerat numquam illum autem autem natus magni amet recusandae labore nihil id sed optio sed animi aut ducimus ipsum ab quas illum possimus sit a",
+                "author": {
+                    "id": 2,
+                    "firstName": "Jaqueline",
+                    "lastName": "Haley",
+                    "bio": "explicabo occaecati enim velit ut unde et sit consectetur esse aliquid et doloremque magnam eum maxime nemo corporis accusantium cumque odio rerum ratione omnis laborum vel impedit atque ad sunt repellendus ut earum a harum doloremque aut eveniet facere sit modi reiciendis deserunt delectus ipsam",
+                    "website": "https://mariah.name"
+                }
+            },
+            {
+                "id": 2,
+                "title": "Dinar out-of-the-box",
+                "date": "2021-04-01T06:24:51.971Z",
+                "authorId": 3,
+                "body": "eius possimus ipsa quibusdam qui ab est voluptatem cum et sint modi magnam magnam sunt ut eos accusantium quaerat temporibus quis ipsa rerum qui debitis error ab eaque vero excepturi qui dolor assumenda sunt omnis esse iste sequi error esse dolor excepturi id dolores aut dolor quia doloribus aut necessitatibus laboriosam quo iste ut accusantium tempore consequatur dolores et magnam consectetur fuga sed perspiciatis minima assumenda nulla rerum omnis deserunt mollitia et natus sit voluptatem ab beatae enim iste quo sit eaque et",
+                "author": {
+                    "id": 3,
+                    "firstName": "Lorena",
+                    "lastName": "Rogahn",
+                    "bio": "quam",
+                    "website": "http://leo.net"
+                }
+            },
+            {
+                "id": 3,
+                "title": "Persevering Practical",
+                "date": "2021-02-28T11:53:24.405Z",
+                "authorId": 2,
+                "body": "quae repellendus molestiae deleniti aperiam consequatur fuga repellat omnis similique qui fugit voluptas voluptatibus vel dolorem molestias nisi repudiandae voluptas libero enim molestiae eius eaque nesciunt officia fugit",
+                "author": {
+                    "id": 2,
+                    "firstName": "Jaqueline",
+                    "lastName": "Haley",
+                    "bio": "explicabo occaecati enim velit ut unde et sit consectetur esse aliquid et doloremque magnam eum maxime nemo corporis accusantium cumque odio rerum ratione omnis laborum vel impedit atque ad sunt repellendus ut earum a harum doloremque aut eveniet facere sit modi reiciendis deserunt delectus ipsam",
+                    "website": "https://mariah.name"
+                }
+            },
+            {
+                "id": 4,
+                "title": "Tanzania",
+                "date": "2021-08-13T21:23:14.747Z",
+                "authorId": 2,
+                "body": "ipsam voluptatem ipsa quos sint consequuntur maiores in tenetur quia temporibus accusamus eius consectetur quae error enim voluptatibus porro ipsam velit minus dolorem quis optio veniam",
+                "author": {
+                    "id": 2,
+                    "firstName": "Jaqueline",
+                    "lastName": "Haley",
+                    "bio": "explicabo occaecati enim velit ut unde et sit consectetur esse aliquid et doloremque magnam eum maxime nemo corporis accusantium cumque odio rerum ratione omnis laborum vel impedit atque ad sunt repellendus ut earum a harum doloremque aut eveniet facere sit modi reiciendis deserunt delectus ipsam",
+                    "website": "https://mariah.name"
+                }
+            }
+        ]
+    };
+}
---
+++ api/Authors/index.js
@@ -0,0 +1,42 @@
+module.exports = async function (context, req) {
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: [
+            {
+                "id": 0,
+                "firstName": "Genevieve",
+                "lastName": "Bogan",
+                "bio": "quia autem tempora quo fugiat natus veniam quidem modi nihil doloremque ea excepturi nihil modi natus nihil neque aliquam totam et animi facilis ut molestias a natus",
+                "website": "https://emmie.net"
+            },
+            {
+                "id": 1,
+                "firstName": "Lottie",
+                "lastName": "Koelpin",
+                "bio": "maxime enim voluptas aut dolor aut inventore nostrum est architecto aut a provident maiores quod et eius similique id tenetur eum rerum voluptatem dolores commodi",
+                "website": "http://kaley.net"
+            },
+            {
+                "id": 2,
+                "firstName": "Jaqueline",
+                "lastName": "Haley",
+                "bio": "explicabo occaecati enim velit ut unde et sit consectetur esse aliquid et doloremque magnam eum maxime nemo corporis accusantium cumque odio rerum ratione omnis laborum vel impedit atque ad sunt repellendus ut earum a harum doloremque aut eveniet facere sit modi reiciendis deserunt delectus ipsam",
+                "website": "https://mariah.name"
+            },
+            {
+                "id": 3,
+                "firstName": "Lorena",
+                "lastName": "Rogahn",
+                "bio": "quam",
+                "website": "http://leo.net"
+            },
+            {
+                "id": 4,
+                "firstName": "Maegan",
+                "lastName": "Jakubowski",
+                "bio": "inventore quae neque quia impedit omnis repudiandae est id aut repellat exercitationem esse omnis velit facere quae dicta esse eveniet ipsum fugit ut dignissimos at non sit modi et laborum alias et dolorum ut quidem sint numquam sed aliquid",
+                "website": "http://summer.name"
+            }
+        ]
+    };
+}
