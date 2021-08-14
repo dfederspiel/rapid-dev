b31aed8d79cb497782d78eb7e6ec5ff2ce1a5131
David Federspiel <david@federnet.com>
2021-08-14 01:52:17 -0400

    feat: add id filter for detail page

--- api/Articles/index.js
+++ api/Articles/index.js
@@ -1,8 +1,9 @@
 var jsonData = require('../generate');
 
 module.exports = async function (context, req) {
+    const byId = (req.query.id);
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: jsonData().articles
+        body: byId ? jsonData().articles[byId] : jsonData().articles
     };
 }
